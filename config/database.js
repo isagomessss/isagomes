6fbedf8eaa9593286d3168dda2c4c8ada49e7bbf
Isadora Gomes Pinheiro <gomespinheiroisadora@gmail.com>
2025-09-17 17:16:18 -0300

    Atualizações

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'auth_session',
   process.env.DB_USER || 'root',
-  process.env.DB_PASS || '',
+  process.env.DB_PASSWORD || '1147890h',
   {
     host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql',
